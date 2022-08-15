0ae0b4bf23829a0c759d2c52fcf751d1b937c016
adkatarine <adllakatarine@hotmail.com>
2022-08-15 10:37:30 -0300

    refactor: nome de categoria alterado para unico na migration

--- database/migrations/2022_08_14_000217_create_categories_table.php
+++ database/migrations/2022_08_14_000217_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->nullable();
+            $table->string('name', 50)->unique()->nullable();
             $table->timestamps();
         });
     }
