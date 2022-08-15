71006c698afb719bf44efe40e40d17b8e1a1c590
adkatarine <adllakatarine@hotmail.com>
2022-08-15 10:59:13 -0300

    feat: adiciona interface base do repository

---
+++ app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface BaseRepositoryInterface {
+    public function create(array $data);
+    public function update(int $id, array $data);
+    public function findAll(string $relation = '');
+    public function findById(int $id, string $relation = '');
+    public function delete(int $id);
+}
