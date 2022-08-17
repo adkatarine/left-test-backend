497ad30f9146731b2b5fe9bda1a12c7d940d74e6
adkatarine <adllakatarine@hotmail.com>
2022-08-17 11:06:28 -0300

    config: configura service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace App\Providers;
 
-use App\Repositories\Contracts\BaseRepositoryInterface;
-use App\Repositories\BaseRepository;
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\ClientRepositoryInterface;
+use App\Repositories\Contracts\AddressRepositoryInterface;
+use App\Repositories\CategoryRepository;
+use App\Repositories\ProductRepository;
+use App\Repositories\ClientRepository;
+use App\Repositories\AddressRepository;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\Client;
+use App\Models\Address;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,10 +25,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, function() {
+            return new CategoryRepository(new Category());
+        });
 
-        $this->app->bind(BaseRepositoryInterface::class, function () {
-            return new BaseRepository(new Category());
+        $this->app->bind(ProductRepositoryInterface::class, function() {
+            return new ProductRepository(new Product());
+        });
+
+        $this->app->bind(ClientRepositoryInterface::class, function() {
+            return new ClientRepository(new Client());
+        });
+
+        $this->app->bind(AddressRepositoryInterface::class, function() {
+            return new AddressRepository(new Address());
         });
     }
 
